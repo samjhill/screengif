4153105876bc143e128f73c4c7e7c62e05ce7bc2
Sam J Hill <samuhill@gmail.com>
2016-12-01 09:14:04 -0500

    Create screengif.sh

---
+++ screengif.sh
@@ -0,0 +1,10 @@
+if [ "$1" = "" ] ;
+then
+	echo "Please pass a QuickTime movie file."
+else
+	export fspec="$1"
+	fname=`basename $fspec`
+	fname="${fname%.*}"
+	filedir=$(dirname "$1")
+	ffmpeg -i "$1" -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > $filedir/$fname.gif
+fi
