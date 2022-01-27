358e28c2e56e7ce6d098e29369ebd3398895c662
Sam Hill <samuhill@gmail.com>
2022-01-27 18:22:41 -0500

    tabs -> spaces

--- screengif.sh
+++ screengif.sh
@@ -9,5 +9,5 @@ else
 	ffmpeg -i "$1" -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > $filedir/$fname.gif
 
 	osascript -e 'display notification "Finished converting your screen recording to a gif" with title "Converted gif now available"'
-    osascript -e "tell application \"Finder\"" -e activate -e "reveal POSIX file \"$filedir/$fname.gif\"" -e end tell
+	osascript -e "tell application \"Finder\"" -e activate -e "reveal POSIX file \"$filedir/$fname.gif\"" -e end tell
 fi
